f74cc22bc710173bded197ad4fed90b0210a47a5
Thodoris Sairoglou <thodoriss@superup.io>
2022-09-05 18:52:47 +0300

    Fixes

--- BE/thesis/database.py
+++ BE/thesis/database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHAMY_DATABASE_URL = 'mysql+pymysql://root@localhost:3306/diploma'
+SQLALCHAMY_DATABASE_URL = 'mysql+pymysql://mypubs:uDr4TR6XT39k@localhost:3306/mypubs'
+# SQLALCHAMY_DATABASE_URL = 'mysql+pymysql://root@localhost:3306/mypubs'
 
 engine = create_engine(SQLALCHAMY_DATABASE_URL)
 
